414a1e1fce004c9d714f339cc14cafc756287081
Josh Baker <JoshBaker529@Gmail.com>
2025-06-30 14:34:23 -0400

    Added Todo_Item class

---
+++ src/todo_item.h
@@ -0,0 +1,30 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//        Todo_Item Class
+//
+//        This class is a single todo list item
+//        NOTE: May change to struct
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef TODO_ITEM
+#define TODO_ITEM
+
+#include <string>
+using std::string;
+
+struct dateTime {
+  short year, month, day, hour, minute;
+};
+
+class Todo_Item {
+public:
+  Todo_Item(string name = "", string description = "",
+            dateTime dateTime = {0, 0, 0, 0, 0});
+  string name, description;
+  dateTime dateTime;
+};
+
+#endif // !TODO_ITEM
+#define TODO_ITEM
